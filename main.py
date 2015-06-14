7473b1b8189f54e1d0036c299a3061567482ed6a
Jan-Pascal van Best <janpascal@vanbest.org>
2015-06-14 23:05:48 +0200

    Now using twisted adbapi, no orm. Not complete yet

--- main.py
+++ main.py
@@ -1,32 +1,35 @@
+from datetime import datetime
+import sqlite3
+import time
+
 from twisted.web import xmlrpc, server
 from twisted.web.xmlrpc import withRequest
-from models import Cracker, Report
-from datetime import datetime
-import time
+from twisted.enterprise import adbapi
+
+from models import Cracker, Report, get_cracker
 
 class Example(xmlrpc.XMLRPC):
     """
     An example object to be published.
     """
-    crackers=[]
+
+    def __init__(self, dbpool):
+        xmlrpc.XMLRPC.__init__(self)
+        self.dbpool = dbpool
+
+    def _add_hosts(self, txn, request, hosts):
+        for host in hosts:
+            print("Adding host {}".format(host))
+            cracker = get_cracker(txn, host)
+            cracker.add_report(txn, request.getClientIP())
+            cracker.save(txn)
+
+        return 42
 
     @withRequest
     def xmlrpc_add_hosts(self, request, hosts):
         print("add_hosts({})".format(hosts))
-        for host in hosts:
-            print("Adding host {}".format(host))
-            candidates = [cracker for cracker in self.crackers if
-                cracker.ip_address == host]
-            if len(candidates)>1:
-                print("Error, {} crackers for host {}!".format(len(candidates), host))
-            elif len(candidates)==1:
-                cracker = candidates[0]
-            else:
-                cracker = Cracker(host)
-                self.crackers.append(cracker)
-            cracker.add_report(request.getClientIP())
-
-        return 0
+        return self.dbpool.runInteraction(self._add_hosts, request, hosts)
 
     @withRequest
     def xmlrpc_get_new_hosts(self, request, timestamp, threshold, hosts_added, resiliency):
@@ -37,12 +40,19 @@ class Example(xmlrpc.XMLRPC):
         print("returning: {}".format(result))
         return result
 
-    def xmlrpc_list_all_hosts(self):
-        result = [cracker.ip_address for cracker in self.crackers]
-        return result
+#    def xmlrpc_list_all_hosts(self):
+#        result = [cracker.ip_address for cracker in self.crackers]
+#        return result
 
-    def xmlrpc_dump_database(self):
-        return self.crackers
+#    def xmlrpc_dump_database(self):
+#        return self.crackers
+
+    def _get_cracker_info(self, txn, ip_address):
+        cracker = get_cracker(txn, ip_address)
+        return cracker
+
+    def xmlrpc_get_cracker_info(self, ip_address):
+        return self.dbpool.runInteraction(self._get_cracker_info, ip_address)
 
     @withRequest
     def xmlrpc_ip(self, request):
@@ -70,7 +80,20 @@ class Example(xmlrpc.XMLRPC):
         raise xmlrpc.Fault(123, "The fault procedure is faulty.")
 
 if __name__ == '__main__':
+    print("Creating tables")
+    db = sqlite3.connect("denyhosts.sqlite")
+    db.execute("DROP TABLE IF EXISTS crackers")
+    db.execute("CREATE TABLE crackers (id INTEGER PRIMARY KEY AUTOINCREMENT, ip_address TEXT, first_time INTEGER, latest_time INTEGER, total_reports INTEGER)")
+    db.execute("DROP INDEX IF EXISTS cracker_ip_address")
+    db.execute("CREATE INDEX cracker_ip_address ON crackers (ip_address)")
+    db.execute("DROP TABLE IF EXISTS reports")
+    db.execute("CREATE TABLE reports(id INTEGER PRIMARY KEY AUTOINCREMENT, cracker_id INTEGER, ip_address TEXT, first_report_time INTEGER, latest_report_time INTEGER)")
+    db.close()
+    dbpool = adbapi.ConnectionPool("sqlite3", 'denyhosts.sqlite')
     from twisted.internet import reactor
-    r = Example()
+    r = Example(dbpool)
     reactor.listenTCP(8000, server.Site(r))
+    print("Starting reactor...")
     reactor.run()
+    dbpool.close()
+
--- models.py
+++ models.py
@@ -7,37 +7,74 @@ class Cracker():
         self.first_time = datetime.now()
         self.latest_time = self.first_time
         self.total_reports = 0
-        self.reports = []
+        #self.reports = []
 
-    def add_report(self, ip_address):
+    def add_report(self, txn, ip_address):
         self.latest_time = datetime.now()
         self.total_reports += 1
-        report = self.find_report(ip_address)
-        if report is None:
-            report = Report(ip_address)
-            self.reports.append(report)
+        report = get_report_for_cracker(txn, self, ip_address)
         report.latest_report_time = datetime.now();
+        report.save(txn)
 
     def resiliency(self):
         return self.latest_time - self.first_time
 
-    def current_reports(self):
-        return len(self.reports)
+    #def current_reports(self):
+    #    return len(self.reports)
 
-    def find_report(self, ip_address):
-        candidates = [report for report in self.reports if
-            report.ip_address == ip_address]
-        if len(candidates)>1:
-            print("Error, {} reports for host {} for cracker {}!".format(len(candidates), ip_address, self.ip_address))
-            result = candidates[0]
-        elif len(candidates)==1:
-            result = candidates[0]
+    #def find_report(self, ip_address):
+    #    candidates = [report for report in self.reports if
+    #        report.ip_address == ip_address]
+    #    if len(candidates)>1:
+    #        print("Error, {} reports for host {} for cracker {}!".format(len(candidates), ip_address, self.ip_address))
+    #        result = candidates[0]
+    #    elif len(candidates)==1:
+    #        result = candidates[0]
+    #    else:
+    #        result = None
+    #    return result
+
+    # Must be in DB already
+    def save(self, txn):
+        try:
+            txn.execute("UPDATE crackers SET first_time=?, latest_time=?, total_reports=? WHERE ip_address=?", (self.first_time, self.latest_time, self.total_reports, self.ip_address))
+        except:
+            print("Error in UPDATE")
+        print("Saved cracker {}".format(self.ip_address))
+
+def get_cracker(txn, ip_address):
+        txn.execute("SELECT id, ip_address, first_time, latest_time, total_reports FROM crackers WHERE ip_address=?", (ip_address,))
+        result = txn.fetchone()
+        if result:
+            cracker = Cracker(result[1])
+            cracker.id = result[0]
+            cracker.first_time = result[2]
+            cracker.latest_time = result[3]
+            cracker.total_reports = result[4]
         else:
-            result = None
-        return result
+            cracker = Cracker(ip_address)
+            txn.execute("INSERT INTO crackers (ip_address,first_time,latest_time,total_reports) VALUES (?,?,?,?)",
+            (ip_address,cracker.first_time, cracker.latest_time, cracker.total_reports))
+            cracker.id = txn.lastrowid
+        return cracker
+
+def get_all_crackers(txn):
+        txn.execute("SELECT ip_address, first_time, latest_time, total_reports FROM crackers")
+        results = txn.fetchall()
+        all = []
+        if results:
+            for r in results:
+                cracker = Cracker(r[0])
+                cracker.first_time = r[1]
+                cracker.latest_time = r[2]
+                cracker.total_reports = r[3]
+                all.append(cracker)
+        else:
+            return []
 
 class Report():
-    def __init__(self, ip_address):
+    def __init__(self, cracker, ip_address):
+        self.cracker_id = cracker.id
         self.ip_address = ip_address
         self.first_report_time = datetime.now()
         self.latest_report_time = self.first_report_time
@@ -45,3 +82,25 @@ class Report():
     def latest_total_resiliency(self):
         return self.latest_report_time - self.first_report_time
 
+    def save(self,txn):
+        txn.execute("""UPDATE reports 
+            SET first_report_time=?, latest_report_time=? 
+            WHERE cracker_id=? AND ip_address=?""", 
+            (self.first_report_time, self.latest_report_time, self.cracker_id, self.ip_address))
+
+def get_report_for_cracker(txn, cracker, report_ip_address):
+    txn.execute("""SELECT first_report_time,latest_report_time
+        FROM reports 
+        WHERE reports.cracker_id = ? AND reports.ip_address = ?""", 
+        (cracker.id,report_ip_address))
+    result = txn.fetchone()
+    if result is None:
+        report = Report(cracker, report_ip_address)
+        txn.execute("INSERT INTO reports (cracker_id, ip_address, first_report_time, latest_report_time) VALUES (?,?,?,?)", (cracker.id,
+            report_ip_address, report.first_report_time, report.latest_report_time))
+        return report
+    else:
+        report = Report(cracker,report_ip_address)
+        report.first_report_time = result[0]
+        report.latest_report_time = result[1]
+        return report
