d75308ccee10951aef834ae69e8abc7e851c00cb
Jan-Pascal van Best <janpascal@vanbest.org>
2015-07-02 21:17:52 +0200

    Fix setup script, move to setuptools

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 from glob import glob
 from dh_syncserver import version
 
@@ -13,11 +13,25 @@ setup(name='dh_syncserver',
       author_email='janpascal@vanbest.org',
       url='http://www.github.com/janpascal/denyhosts_sync_server',
       packages=['dh_syncserver'],
-      requires=["twisted.enterprise.adbapi", "twistar"],
+      install_requires=["Twisted", "twistar"],
       scripts=['scripts/dh_syncserver'],
       data_files=[
         (etcpath, glob("dh_syncserver.conf")),
       ],
-      license="AGPL",
+      license="""
+Copyright (C) 2015 Jan-Pascal van Best <janpascal@vanbest.org>
 
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
      )
