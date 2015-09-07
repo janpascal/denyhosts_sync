bc0e55773c095ddb343146b567ef1c6c1cf2e545
Jan-Pascal van Best <janpascal@vanbest.org>
2015-09-07 21:51:46 +0200

    Added stats test to coverage script

--- coverage.sh
+++ coverage.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-python-coverage run /usr/bin/trial tests/test_get_new_hosts.py tests/test_models.py tests/test_purge_methods.py
+python-coverage run /usr/bin/trial \
+    tests/test_get_new_hosts.py \
+    tests/test_models.py \
+    tests/test_purge_methods.py \
+    tests/test_stats.py
 python-coverage html
